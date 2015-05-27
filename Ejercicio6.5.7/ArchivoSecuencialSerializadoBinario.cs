1737c9823f70e20ff94e4cb07f856809eee262f1
Israel Calderon <icalderond@outlook.com>
2015-05-27 17:52:07 -0500

    correcion en el metodo crear

--- Ejercicio6.5.7/ArchivoSecuencialSerializadoBinario.cs
+++ Ejercicio6.5.7/ArchivoSecuencialSerializadoBinario.cs
@@ -43,10 +43,6 @@ namespace Ejercicio6._5._7
 
         private void Crear()
         {
-            if (flujo == null)
-            {
-                flujo = new FileStream();
-            }
         }
 
         public void AbrirEnModoEscritura()
